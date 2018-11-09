703a9d5b854224bb976f46ff39b92c53767d3abe
korakij <41503977+korakij@users.noreply.github.com>
2018-11-09 11:15:14 +0700

    create a sample console app

---
+++ MNG.Utilities.Samples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNG.Utilities.Samples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.AppendFile("charges.csv", "C-181109-01", DateTime.Now, 100.20);
+            Util.AppendFile("charges.csv", "C-181109-02", DateTime.Now, 200.10);
+            Util.AppendFile("charges.csv", "C-181109-03", DateTime.Now, 120.20);
+        }
+    }
+}
